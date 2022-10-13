e62017da381f0fdcf27b7ea5ccfb43670bf2a690
daehwan0307 <eoghks960307@naver.com>
2022-10-13 21:00:05 +0900

    change class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,25 @@ import domain.Hospital;
 import parser.HospitalParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        FileController<Hospital> hospitalFileController = new FileController<>(new HospitalParser());
-        String filename =   "C:\\Users\\daehwan\\Desktop\\ÏÑúÏö∏Ïãú Î≥ëÏùòÏõê ÏúÑÏπò Ï†ïÎ≥¥.csv";
+        boolean a=true;
+        FileController<Hospital> hospitalFileController = new FileController<>(new HospitalParser(),a);
+        String filename =   "C:\\Users\\daehwan\\Desktop\\º≠øÔΩ√ ∫¥¿«ø¯ ¿ßƒ°¡§∫∏.csv";
         List<Hospital> hospitals = hospitalFileController.readLines(filename);
 
         System.out.println(hospitals.size());
+        List<String> lines = new ArrayList<>();
         for (Hospital hospital : hospitals) {
-            System.out.printf("%s,%s,%s,%s,%d,%s,%s\n",hospital.getId(),hospital.getAddress(),
-                    hospital.getDistrict(),hospital.getCategory(),
-                    hospital.getEmergencyRoom(),hospital.getName(),
-                    hospital.getSubdivision());
+            lines.add(hospital.getSqlInsertQuery());
         }
+        String sqlFilename = "hospital_insert.sql";
+        hospitalFileController.createANewFile(sqlFilename);
+        hospitalFileController.writeLines(lines, sqlFilename);
     }
 }
--- src/main/java/parser/HospitalParser.java
+++ src/main/java/parser/HospitalParser.java
@@ -4,17 +4,15 @@ import domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital>{
 
-    private String getSubdivision(String name){
-        String[] suvdivisions = { "소아과", "피부과", "성형외과", "정형외과",  "산부인과",  "안과", "가정의학과", "비뇨기과", "치과","내과", "외과"};
+    private String getSubdivision(String name) {
+        String[] subdivisions = {"소아과", "피부과", "성형외과", "정형외과", "산부인과", "관절", "안과", "가정의학과", "비뇨기과", "치과", "내과", "외과"};
 
-        for(String suvdivision : suvdivisions){
-            if(name.contains(suvdivision)){
-                return suvdivision;
+        for (String subdivision : subdivisions) {
+            if (name.contains(subdivision)) {
+                return subdivision;
             }
-
         }
         return "";
-
     }
     @Override
     public Hospital parse(String str) {
