524e2924cebc675703acdd3492a243c990ddd2cf
daehwan0307 <eoghks960307@naver.com>
2022-10-13 14:16:16 +0900

    check category , emergencyRoom, name parsing

--- src/main/java/parser/HospitalParser.java
+++ src/main/java/parser/HospitalParser.java
@@ -10,7 +10,11 @@ public class HospitalParser implements Parser<Hospital>{
         String[] splitted = str.split(",");
 //      address;
 //      district;
-
-        return new Hospital(splitted[0],splitted[1],splitted[2],Integer.parseInt(splitted[6]),splitted[4],splitted[5]);
+//      category;
+//      Integer emergencyRoom;
+//      name;
+//      subdivision;
+        // subdivision은 파싱을 해서 넣는 것이 좋을 것 같다.
+        return new Hospital(splitted[0],splitted[1],splitted[2],Integer.parseInt(splitted[6]),splitted[10],splitted[10]);
     }
 }
