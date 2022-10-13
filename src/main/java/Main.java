114c098c2fd418508350b3c1d6ab85be86477a44
daehwan0307 <eoghks960307@naver.com>
2022-10-13 13:14:59 +0900

    check address parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
         LineReader<Hospital> hospitalLineReader = new LineReader<>(new HospitalParser());
-        String filename = "C:\\Users\\daehwan\\Desktop\\서울시 병의원 위치 정보.csv";
+        String filename =   "C:\\Users\\daehwan\\Desktop\\서울시 병의원 위치 정보.csv";
         List<Hospital> hospitals = hospitalLineReader.readLines(filename);
 
         System.out.println(hospitals.size());
--- src/main/java/domain/Hospital.java
+++ src/main/java/domain/Hospital.java
@@ -2,6 +2,37 @@ package domain;
 
 public class Hospital {
     private String id;
+
+    public Hospital(String id, String address) {
+        this.id = id;
+        this.address = address;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Integer getEmergencyRoom() {
+        return emergencyRoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubdivision() {
+        return subdivision;
+    }
+
     private String address;
     private String district;
     private String category;
