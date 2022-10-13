4b88d19e9b3f86c27a6a1e62253033c83057276c
daehwan0307 <eoghks960307@naver.com>
2022-10-13 15:34:45 +0900

    refactoring test code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FileController<Hospital> hospitalLineReader = new FileController<>(new HospitalParser());
+        FileController<Hospital> hospitalFileController = new FileController<>(new HospitalParser());
         String filename =   "C:\\Users\\daehwan\\Desktop\\서울시 병의원 위치 정보.csv";
-        List<Hospital> hospitals = hospitalLineReader.readLines(filename);
+        List<Hospital> hospitals = hospitalFileController.readLines(filename);
 
         System.out.println(hospitals.size());
         for (Hospital hospital : hospitals) {
-            System.out.println(hospital.getId());
+            System.out.printf("%s,%s,%s,%s,%d,%s,%s\n",hospital.getId(),hospital.getAddress(),
+                    hospital.getDistrict(),hospital.getCategory(),
+                    hospital.getEmergencyRoom(),hospital.getName(),
+                    hospital.getSubdivision());
         }
     }
 }
