148864619a727b158675009c67ddd27c885f6a74
daehwan0307 <eoghks960307@naver.com>
2022-10-19 13:07:09 +0900

    factory적용

---
+++ src/main/java/com/dao/UserDaoFactory.java
@@ -0,0 +1,14 @@
+package com.dao;
+
+public class UserDaoFactory {
+    public UserDao awsUserDao(){
+        AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
+        UserDao userDao = new UserDao(awsConnectionMaker);
+        return userDao;
+    }
+
+    public UserDao localUserDao(){
+        UserDao userDao = new UserDao(new LocalConnectionMaker());
+        return userDao;
+    }
+}
