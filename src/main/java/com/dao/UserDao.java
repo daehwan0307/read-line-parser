674f45bb846c8900bb3bdcb233c228138de6594d
daehwan0307 <eoghks960307@naver.com>
2022-10-20 13:13:29 +0900

    Add deleteall(), getCount()

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -65,6 +65,24 @@ public class UserDao {
 
         return user;
     }
+    public void deleteAll()throws SQLException{
+        Connection conn = connectionMaker.makeConnection();
+        PreparedStatement ps = conn.prepareStatement("delete from users");
+        ps.executeUpdate();
+        ps.close();
+        conn.close();
+    }
+    public int getCount() throws  SQLException{
+        Connection conn = connectionMaker.makeConnection();
+        PreparedStatement ps = conn.prepareStatement("select count(*) from users");
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+        rs.close();
+        ps.close();
+        conn.close();
+        return count;
+    }
 
     public static void main(String[] args) throws SQLException,ClassNotFoundException {
         UserDao userDao = new UserDao();
